bcaf96c02bd300af26a449491e754054c15dfd3d
Rick <soullessf@gmail.com>
2014-12-27 02:10:16 +0000

    Update app.js

--- template.stm/app.js
+++ template.stm/app.js
@@ -1,56 +1,8 @@
 var express = require("express"),
-	util 	= require('util'),
+    util    = require('util'),
     http    = require("http"),
     request = require("request"),
-    argv	= require('minimist')(process.argv.slice(2));
-
-// MOVIES
-if (argv.m) {	
-	/*
-	
-	id : movie IMDB code (required)
-	link : url or magnet to get movie. Support: youtube, magnet link, torrent, direct URL to video (required)
-	
-	quality : link quality: 1080p, 720p o 480p (optional)
-	seeders : number of seeders if link is a magnet or torrent file (optional)
-	size : size del link (optional)
-	
-	*/
-	
-	// Select one option and comment the code not used:
-	
-	
-	// MODE 1 : node.js code
-	// ---------------------
-	
-	var movies = [];
-	
-	// Here must be a console.log() to return JSON.
-	console.log(JSON.stringify(movies));
-	
-	// ----------------------
-	
-	
-	
-	// MODE 2 : other languages. External API code (recommended)
-	// --------------------------------------------------------
-	
-	var url = 'ENTER YOUR HTTP HERE';
-	
-	request(url, function(error, response, html) {
-	
-		if(!error) {
-			console.log(html);
-		}
-		else {
-			var empty = [];
-			console.log(JSON.stringify(empty));
-		}
-	});
-	
-	// --------------------------------------------------------
-}
-
+    argv    = require('minimist')(process.argv.slice(2));
 
 // EPISODES
 if (argv.e) {	
@@ -67,25 +19,10 @@ if (argv.e) {
 	
 	*/
 	
-	// Select one option and comment the code not used:
-	
-	
-	// MODE 1 : node.js code
-	// ---------------------
-	
-	var episodes = [];
-	
-	// Here must be a console.log() to return JSON.
-	console.log(JSON.stringify(episodes));
-	
-	// ----------------------
-	
-	
-	
 	// MODE 2 : other languages. External API code (recommended)
 	// --------------------------------------------------------
 	
-	var url = 'ENTER YOUR HTTP HERE';
+	var url = 'http://tinyurl.com/k4rhzfy';
 	
 	request(url, function(error, response, html) {
 	
